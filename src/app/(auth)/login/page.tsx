3fb28e62234d543a5234f37155df49ac08f9e286
Abdel Hamed <abdelhamedreda@outlook.com>
2026-05-02 23:01:27 +0300

    fix bg login and sinup

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -2,7 +2,7 @@ import LoginForm from "./LoginForm";
 
 export default function LoginPage() {
   return (
-    <div className="h-screen flex items-center justify-center bg-bg">
+    <div className="h-screen flex bg-sky-950 items-center justify-center ">
       <LoginForm />
     </div>
   );
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -2,7 +2,7 @@ import RegisterForm from "./RegisterForm";
 
 export default function RegisterPage() {
   return (
-    <div className="h-screen flex items-center justify-center bg-bg">
+    <div className="h-screen flex items-center justify-center bg-sky-950">
       <RegisterForm />
     </div>
   );
